b528186c96b20908a8280d0f69f79ce64dd80d06
AMANmohd1 <mohdaman2001@gmail.com>
2023-09-29 18:49:06 +0530

    Added the api folder

--- SGToolWindow/ToolWindow1Control.xaml.cs
+++ SGToolWindow/ToolWindow1Control.xaml.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
+
 
 namespace SGToolWindow
 {
@@ -25,11 +27,16 @@ namespace SGToolWindow
         {
             string userMessage = UserInput.Text;
 
+            AddMessage("User", userMessage);
+
             // Call your API to get the chatbot response
-            //string chatbotResponse = await GetChatbotResponse(userMessage);
+            string chatbotResponse = await GetChatbotResponse(userMessage);
 
             // Display the chatbot response in the ChatHistory TextBlock
-            AddMessage("User", userMessage);
+            AddMessage("Chatbot", chatbotResponse);
+
+            //Placeholder chatbot response for test
+            //string chatbotResponse = "Placeholder GPT response";
             //AddMessage("Chatbot", chatbotResponse);
 
             // Clear the user input
@@ -38,19 +45,32 @@ namespace SGToolWindow
 
         private void AddMessage(string sender, string message)
         {
-            // Create a new TextBlock to display the message.
-            TextBlock messageBlock = new TextBlock();
+            // Create a new Border to act as the chat bubble.
+            Border chatBubble = new Border();
+            chatBubble.Background = (sender == "User") ? Brushes.LightBlue : Brushes.LightGray;
+            chatBubble.BorderBrush = Brushes.Gray;
+            chatBubble.BorderThickness = new Thickness(1);
+            chatBubble.CornerRadius = new CornerRadius(10);
+            chatBubble.Padding = new Thickness(8);
+            chatBubble.Margin = new Thickness(5, 5, 5, 0); // Add margin for spacing.
+
+            TextBox messageBlock = new TextBox();
             messageBlock.Text = $"{sender}: {message}";
+            messageBlock.IsReadOnly = true;
             messageBlock.TextWrapping = TextWrapping.Wrap;
+            messageBlock.Background = Brushes.Transparent;
+            messageBlock.BorderThickness = new Thickness(0);
+
+            chatBubble.Child = messageBlock;
 
             // Add the TextBlock to the chat history.
-            ChatHistory.Children.Add(messageBlock);
+            ChatHistory.Children.Add(chatBubble);
 
             // Scroll to the bottom of the chat history to show the latest message.
             ChatHistoryScrollViewer.ScrollToBottom();
         }
 
-        private readonly string apiUrl = "http://localhost:5000/chatbot"; // Update with your API URL
+        private readonly string apiUrl = "http://127.0.0.1:5000/chatbot"; // Update with your API URL
 
         public async Task<string> GetChatbotResponse(string userInput)
         {
